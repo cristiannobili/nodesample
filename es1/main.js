b54721706730c649322cc88b0ebc0e2045a691f1
Cristian Nobili <c.nobili@gmail.com>
2024-01-09 19:39:42 +0100

    fetch

--- es1/main.js
+++ es1/main.js
@@ -1,3 +1,52 @@
 import fetch from 'node-fetch';
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const fs = require('fs');
+const readline = require('readline');
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-fetch("https://www.google.com").then(data => data.text()).then(console.log);
+const conf = JSON.parse(fs.readFileSync('conf.json'));
+const token = conf.token;
+const baseUrl = "https://ws.progettimolinari.it";
+const urlSet = baseUrl + "/cache/set";
+
+const readText = (question) => {
+   return new Promise((resolve) => {
+      readline.createInterface({
+         input: process.stdin,
+         output: process.stdout
+      });
+      readline.question(question, text => {
+         readline.close();
+         resolve(text);
+      });
+   });
+}
+
+const saveData = (key, value) => {
+   return new Promise((resolve, reject) => {
+      fetch(urlSet, {
+         headers: {
+            'Content-Type': 'application/json',
+            'key': token
+         },
+         method: "POST",
+         body: JSON.stringify({
+            key: key,
+            value: value
+         })
+      }).then(r => r.json())
+         .then(resolve)
+         .catch(reject)
+      })
+   }
+
+
+readText("Inserisci la chiave: ").then((response) => {
+   const key = response;
+   readText("Inserisci il valore: ").then((response) => {
+      const value = response;
+      saveData(key, value).then(console.log);      
+   })
+});
+
